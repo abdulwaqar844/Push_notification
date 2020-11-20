eb4a79bb3aeff145f998ba925a99c89d8674e4a3
Abdul Waqar <abdulwaqar844@gmaill.com>
2020-11-21 00:25:27 +0500

    Css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {initNotification} from "./Services/firebaseService"
+import {initNotification} from "./Services/firebaseService";
+import "./App.css"
 function App() {
   return (
     <div className="App">
